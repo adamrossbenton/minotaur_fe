c4f97c5712fe83f11c0c3e865475ddc30707427c
adamrossbenton <adamrossbenton@gmail.com>
2022-01-06 10:17:50 -0800

    Admin hooks, login & signup pages started

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react"
+import {useHistory, Link} from "react-router-dom"
+import PropTypes from "prop-types"
+
+const Login = props => {
+    
+    return <>
+        <h1>hello login</h1>
+    </>
+}
+
+export default Login
