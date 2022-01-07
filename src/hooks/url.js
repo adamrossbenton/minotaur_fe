f970202ed31365d0e559fb15d8444a48a769beac
adamrossbenton <adamrossbenton@gmail.com>
2022-01-07 13:38:08 -0800

    basic functionality works with deployed backend

--- src/hooks/url.js
+++ src/hooks/url.js
@@ -1,3 +1,3 @@
-// export default "https://minotaurbackend.herokuapp.com/"
+export default "https://minotaurbackend.herokuapp.com/"
 
-export default "http://localhost:4000/"
+// export default "http://localhost:4000/"
